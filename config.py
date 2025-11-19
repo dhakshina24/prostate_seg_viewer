51f2f41a6239aa203a274599edc5b386a7ef4cbc
Dhakshina Ilango <f20190003@dubai.bits-pilani.ac.in>
2025-11-19 02:11:19 -0600

    Modified Config File

--- config.py
+++ config.py
@@ -1,8 +1,25 @@
 """Configuration file for input/output paths."""
-import os 
+import os
 
-PROJECT_DIR = "./picai-segmentation-viewer"
+# Get path of current directory (works on all OS: Windows, Linux, Streamlit Cloud)
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Input folder inside repo
 INPUT_DIR = os.path.join(PROJECT_DIR, "test")
+
+# Output folder inside repo
 OUTPUT_DIR = os.path.join(PROJECT_DIR, "output_debug")
-SEG_PATH = os.path.join(OUTPUT_DIR, "images", "cspca-detection-map", "cspca_detection_map.mha")
-SCORE_PATH = os.path.join(OUTPUT_DIR, "cspca-case-level-likelihood.json")
+
+# Segmentation mask path (relative)
+SEG_PATH = os.path.join(
+    OUTPUT_DIR, 
+    "images", 
+    "cspca-detection-map", 
+    "cspca_detection_map.mha"
+)
+
+# Probability JSON (relative)
+SCORE_PATH = os.path.join(
+    OUTPUT_DIR, 
+    "cspca-case-level-likelihood.json"
+)
